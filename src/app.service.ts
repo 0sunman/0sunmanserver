fa618d36d592aae732ed8e1e880a6bc381ba0ba6
0sunman <youyoungsun2000@gmail.com>
2023-11-15 10:29:11 +0900

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '나는 서버다!!!!';
+    return '나는 서버다!!!! 2';
   }
   getTest(): string{
     return 'Hello Test!';

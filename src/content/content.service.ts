cf321bdb1664bb53fa775b4ea7fc44c1cc6ef51e
0sunman <youyoungsun2000@gmail.com>
2023-11-15 17:07:48 +0900

    test

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -28,7 +28,7 @@ export class ContentService {
   }
 
   remove(id: number) {
-    this.contentRepository.softDelete({"id":id});
+    this.contentRepository.delete({"id":id});
     return `This action removes a #${id} content`;
   }
 }
